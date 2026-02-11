3af83bb44bc0340fde078ed799aab23c89fa96f3
MrThakar <swarthakar06@gmail.com>
2026-02-11 00:52:45 -0500

    Implemented piece, building, city, road, and settlement java classes

--- javacatan/src-sim/catansim/Road.java
+++ javacatan/src-sim/catansim/Road.java
@@ -4,9 +4,34 @@
 
 package catansim;
 
-/************************************************************/
 /**
- * 
+ * Represents a road placed on the board.
  */
 public class Road implements Piece {
+	
+	private PlayerID owner;
+	
+	/**
+	 * Constructor for road.
+	 * @param owner The player who owns the road.
+	 */
+	public Road(PlayerID owner) {
+		this.owner = owner;
+	}
+	
+	/**
+	 * Returns the player who owns this road.
+	 */
+	@Override
+	public PlayerID getOwnerPlayerID() {
+		return owner;
+	}
+	
+	/**
+	 * Returns the type of this piece. 
+	 */
+	@Override
+	public BuildingTypes getType() {
+		return BuildingTypes.ROAD;
+	}
 }
