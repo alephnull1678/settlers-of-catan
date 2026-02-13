3f15dccdbd42559b2f69c49b5c5b66e6f1a4f569
alephnull1678 <alexmartsavournin@gmail.com>
2026-02-13 00:57:35 -0500

    added demonstrator class

---
+++ javacatan/src-sim/catansim/Demonstrator.java
@@ -0,0 +1,29 @@
+package catansim;
+
+public class Demonstrator {
+
+	public static void main(String[] args) {
+
+        //Create core components
+        Board board = new HardWiredBoard();
+        Validator validator = new Validator();
+        
+        //Create dice
+        MultiDice dice = new MultiDice();
+        dice.addDice(new RegularDice());
+        dice.addDice(new RegularDice());
+
+        //Create players
+        Player[] players = new Player[] {
+                new Player(PlayerID.Player1),
+                new Player(PlayerID.Player2),
+                new Player(PlayerID.Player3),
+                new Player(PlayerID.Player4)
+        };
+
+        //Run game
+        int maxRounds = 100;
+        Game game = new Game(players, board, validator, dice, maxRounds);
+        game.run();
+    }
+}
