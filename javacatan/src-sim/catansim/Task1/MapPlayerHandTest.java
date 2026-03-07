f33efce2e2561e6c4a777f303df9f9d44a5147be
MrThakar <swarthakar06@gmail.com>
2026-03-07 16:35:54 -0500

    Test MapPlayerHand: verify addCard() increases resource count #16

--- javacatan/src-sim/catansim/Task1/MapPlayerHandTest.java
+++ javacatan/src-sim/catansim/Task1/MapPlayerHandTest.java
@@ -1,5 +1,26 @@
 package catansim.Task1;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import catansim.MapPlayerHand;
+import catansim.MapCatalog;
+import catansim.Resource;
+
+/**
+ * Unit tests for MapPlayerHand.
+ * These tests verify adding cards, removing cards
+ */
 public class MapPlayerHandTest {
     
+    // Tests that adding cards increases the resource count
+    @Test
+    public void test_addCard_increasesCount() {
+
+        MapPlayerHand hand = new MapPlayerHand();
+
+        hand.addCard(Resource.WOOD, 2);
+
+        assertEquals("Player should have 2 wood after adding", 2, hand.getCount(Resource.WOOD));
+    }
 }
