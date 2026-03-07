1d082bc26a31caa657929e53ebe863fc28cbf60f
Lofeeeeeee <logansedore06@gmail.com>
2026-03-07 15:12:57 -0500

    JUnit Setup and Tested (#10)

--- javacatan/src-sim/catansim/Task1/NodeTest.java
+++ javacatan/src-sim/catansim/Task1/NodeTest.java
@@ -6,5 +6,13 @@ import static org.junit.Assert.*;
 import catansim.*;
 
 public class NodeTest {
+	
+	@Test
+	public void testJUnitSetup() {
+		
+		int a = 2 + 2;
+		
+		assertEquals(4, a);
+	}
 
 }
