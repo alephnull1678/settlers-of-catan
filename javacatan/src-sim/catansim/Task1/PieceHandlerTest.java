e0c3271bafc72dfbf1411be1ac5eb82771051c7b
MrThakar <swarthakar06@gmail.com>
2026-03-07 17:51:24 -0500

    Updated PieceHandlerTest #16

--- javacatan/src-sim/catansim/Task1/PieceHandlerTest.java
+++ javacatan/src-sim/catansim/Task1/PieceHandlerTest.java
@@ -1,5 +1,29 @@
 package catansim.Task1;
 
-public class PiecdHandlerTest {
-    
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import catansim.PieceHandler;
+import catansim.PieceTypes;
+import catansim.Piece;
+import catansim.PlayerID;
+
+/**
+ * Unit tests for PieceHandler.
+ * 
+ */
+public class PieceHandlerTest {
+
+    // Tests that a new PieceHandler starts with the correct maximum number of pieces
+    @Test
+    public void test_initialPieceCounts() {
+
+        PieceHandler handler = new PieceHandler(PlayerID.BLUE);
+
+        assertEquals("Roads should start at maximum", PieceHandler.MAX_ROADS, handler.getAvailable(PieceTypes.ROAD));
+        
+        assertEquals("Settlements should start at maximum", PieceHandler.MAX_SETTLEMENTS, handler.getAvailable(PieceTypes.SETTLEMENT));
+
+        assertEquals("Cities should start at maximum", PieceHandler.MAX_CITIES, handler.getAvailable(PieceTypes.CITY));
+    }
 }
