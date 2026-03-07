cf3469203645a13fbc7940ef80acef7f3df59b8a
Lofeeeeeee <logansedore06@gmail.com>
2026-03-07 15:39:44 -0500

    2/3 Unit Test in NodeTest Complete (#17)

--- javacatan/src-sim/catansim/Task1/NodeTest.java
+++ javacatan/src-sim/catansim/Task1/NodeTest.java
@@ -7,19 +7,30 @@ import catansim.*;
 
 // Behaviors to be tested:
 // 		1: Connect to tiles
-//		2: Connect to other nodes
-//		3: Hold buildings
+//		2: Hold buildings
 public class NodeTest {
 	
-	// Test that node does not allow more than 3 tiles
+	// 1: Test that node does not allow more than 3 tiles
 	@Test(expected = IllegalStateException.class)
 	public void testFailOnForthTile() {
 	    Node node = new Node(1);
 
+	    // Adding tiles to one node
 	    node.addTile(new Tile(Resource.WOOD, 4, 1));
 	    node.addTile(new Tile(Resource.BRICK, 5, 2));
 	    node.addTile(new Tile(Resource.SHEEP, 6, 3));
-	    node.addTile(new Tile(Resource.WHEAT, 8, 4));
+	    node.addTile(new Tile(Resource.WHEAT, 8, 4)); //Fail here
+	}
+	
+	// 2: Test that the node can store a building
+	@Test
+	public void testNodeStoresBuilding() {
+		Node node = new Node(10);
+		Settlement settlement = new Settlement(PlayerID.BLUE);
+		
+		node.placeBuilding(settlement); //Places building on node
+		
+		assertEquals(settlement, node.getBuilding()); //Check that node has building 
 	}
 
 }
