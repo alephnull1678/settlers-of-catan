e1a160ab8aa5a4c1046ce8941720d13c45b87223
Lofeeeeeee <logansedore06@gmail.com>
2026-03-07 15:22:58 -0500

    Node Test Updates

--- javacatan/src-sim/catansim/Task1/NodeTest.java
+++ javacatan/src-sim/catansim/Task1/NodeTest.java
@@ -5,14 +5,21 @@ import static org.junit.Assert.*;
 
 import catansim.*;
 
+// Behaviors to be tested:
+// 		1: Connect to tiles
+//		2: Connect to other nodes
+//		3: Hold buildings
 public class NodeTest {
 	
-	@Test
-	public void testJUnitSetup() {
-		
-		int a = 2 + 2;
-		
-		assertEquals(4, a);
+	// Test that node does not allow more than 3 tiles
+	@Test(expected = IllegalStateException.class)
+	public void testFailOnForthTile() {
+	    Node node = new Node(1);
+
+	    node.addTile(new Tile(Resource.WOOD, 4, 1));
+	    node.addTile(new Tile(Resource.BRICK, 5, 2));
+	    node.addTile(new Tile(Resource.SHEEP, 6, 3));
+	    node.addTile(new Tile(Resource.WHEAT, 8, 4));
 	}
 
 }
