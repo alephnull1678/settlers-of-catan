c8770c6c2307cc31f1df467fcd7dd5e696ffff58
alephnull1678 <alexmartsavournin@gmail.com>
2026-03-11 02:11:25 -0400

    added scanner and human player #25

--- javacatan/src-sim/catansim/Demonstrator.java
+++ javacatan/src-sim/catansim/Demonstrator.java
@@ -4,13 +4,16 @@
 
 package catansim;
 
+import java.util.Scanner;
+
 public class Demonstrator {
 
 	public static void main(String[] args) {
 
 		//The game requires a list of all players in the game, the board to be used, the validator used to validate actions, the dice to be used, and the maximum number of rounds.
 		//For this simulation, we will use a hard-wired board that creates the same collection of tiles, resources and dice numbers every time.
-		//We will also use basic Players that will do a linear search through every possible valid move (provided by the Validator) and choose randomly.
+		//We will also use basic Player Agents that will do a linear search through every possible valid move (provided by the Validator) and choose randomly.
+		//Furthermore, we will be using a Human Player that can read player commands and execute them in-game.
 		//We will be sending a MultiDice made up of two dice (the range of the rolls for this sim will be 1-12.)
 		
         //Create board and validator
@@ -22,12 +25,18 @@ public class Demonstrator {
         dice.addDice(new RegularDice());
         dice.addDice(new RegularDice());
 
+        //Create scanner for human input
+        Scanner scanner = new Scanner(System.in);
+
+        //Create state machine
+        StateMachine stateMachine = new StateMachine();
+
         //Create players
         Player[] players = new Player[] {
-                new Player(PlayerID.BLUE),
-                new Player(PlayerID.RED),
-                new Player(PlayerID.YELLOW),
-                new Player(PlayerID.GREEN)
+                new HumanPlayer(PlayerID.BLUE, scanner),
+                new AgentPlayer(PlayerID.RED),
+                new AgentPlayer(PlayerID.YELLOW),
+                new AgentPlayer(PlayerID.GREEN)
         };
 
         //Run the game. The game will end either when a Player reaches 10 victory points or the game reaches the max number of rounds as assigned by maxRounds.
@@ -41,7 +50,9 @@ public class Demonstrator {
         //they do not receive the necessary resources to place any pieces down. In this case, rounds may often reach the maxRounds threshold without a winner. This is to be expected.
         
         int maxRounds = 8192;
-        Game game = new Game(players, board, validator, dice, maxRounds);
+        Game game = new Game(players, board, validator, dice, stateMachine, maxRounds);
         game.run();
+
+        scanner.close();
     }
 }
