866c554811029ddabc3e436ed12ee452623a226e
Lofeeeeeee <logansedore06@gmail.com>
2026-03-11 01:07:36 -0400

    Update BuildAction() to work with parser | Created Unit Tests for Parser (#16)(#12)

--- javacatan/src-sim/catansim/BuildAction.java
+++ javacatan/src-sim/catansim/BuildAction.java
@@ -8,6 +8,7 @@ public class BuildAction extends Action {
     private final Node[] nodes;
     private final PieceTypes pieceType;
 
+    // Existing constructor (used by Validator / Game)
     public BuildAction(Node node, PieceTypes pieceType) {
         super(ActionTypes.BUILD);
 
@@ -22,8 +23,39 @@ public class BuildAction extends Action {
         this.pieceType = pieceType;
     }
 
+    // Existing constructor (used by Validator / Game)
     public BuildAction(Node node1, Node node2, PieceTypes pieceType) {
         super(ActionTypes.BUILD);
+
+        if (node1 == null || node2 == null) {
+            throw new IllegalArgumentException("nodes cannot be null");
+        }
+        if (pieceType == null) {
+            throw new IllegalArgumentException("pieceType cannot be null");
+        }
+
+        this.nodes = new Node[]{node1, node2};
+        this.pieceType = pieceType;
+    }
+
+    // NEW constructor used by Parser
+    public BuildAction(ActionTypes type, Node node, PieceTypes pieceType) {
+        super(type);
+
+        if (node == null) {
+            throw new IllegalArgumentException("node cannot be null");
+        }
+        if (pieceType == null) {
+            throw new IllegalArgumentException("pieceType cannot be null");
+        }
+
+        this.nodes = new Node[]{node};
+        this.pieceType = pieceType;
+    }
+
+    // NEW constructor used by Parser
+    public BuildAction(ActionTypes type, Node node1, Node node2, PieceTypes pieceType) {
+        super(type);
 
         if (node1 == null || node2 == null) {
             throw new IllegalArgumentException("nodes cannot be null");
@@ -51,7 +83,9 @@ public class BuildAction extends Action {
         if (!super.equals(obj)) return false;
 
         BuildAction other = (BuildAction) obj;
-        return pieceType == other.pieceType && Arrays.equals(nodes, other.nodes);
+
+        return pieceType == other.pieceType &&
+               Arrays.equals(nodes, other.nodes);
     }
 
     @Override
