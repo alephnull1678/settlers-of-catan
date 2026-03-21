a6fe004f593f1184477b602be256936a83f7de24
Jalepiii <jalaldevstuff@gmail.com>
2026-03-20 23:27:35 -0400

    Added new game test

--- javacatan/src-sim/catansim/Task1/NodeTest.java
+++ javacatan/src-sim/catansim/Task1/NodeTest.java
@@ -3,6 +3,8 @@ package catansim.Task1;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.util.Scanner;
+
 import catansim.*;
 
 // Behaviors to be tested:
@@ -160,5 +162,60 @@ public class NodeTest {
     	board = new HardWiredBoard();
     	
         assertThrows(IllegalArgumentException.class, () -> board.getLongestRoadLength(null));
+    }
+    
+    @Test
+    public void gameTest() {
+    	//The game requires a list of all players in the game, the board to be used, the validator used to validate actions, the dice to be used, and the maximum number of rounds.
+		//For this simulation, we will use a hard-wired board that creates the same collection of tiles, resources and dice numbers every time.
+		//We will also use basic Player Agents that will do a linear search through every possible valid move (provided by the Validator) and choose randomly.
+		//Furthermore, we will be using a Human Player that can read player commands and execute them in-game.
+		//We will be sending a MultiDice made up of two dice (the range of the rolls for this sim will be 1-12.)
+		
+        //Create board and validator
+        Board board = new HardWiredBoard();
+        Validator validator = new Validator();
+        
+        //Create dice
+        MultiDice dice = new MultiDice();
+        dice.addDice(new RegularDice());
+        dice.addDice(new RegularDice());
+
+        //Create scanner for human input
+        Scanner scanner = new Scanner(System.in);
+
+        //Create state machine
+        StateMachine stateMachine = new StateMachine();
+
+        //Create players
+        /*Player[] players = new Player[] {
+        	    new HumanPlayer(PlayerID.BLUE, scanner),
+        	    new AgentPlayer(PlayerID.RED, new IntelligentDecisionStrategy()),
+        	    new AgentPlayer(PlayerID.WHITE, new IntelligentDecisionStrategy()),
+        	    new AgentPlayer(PlayerID.ORANGE, new IntelligentDecisionStrategy())
+        	};*/
+        
+        Player[] players = new Player[] {
+        	    new AgentPlayer(PlayerID.BLUE, new IntelligentDecisionStrategy()),
+        	    new AgentPlayer(PlayerID.RED, new IntelligentDecisionStrategy()),
+        	    new AgentPlayer(PlayerID.WHITE, new IntelligentDecisionStrategy()),
+        	    new AgentPlayer(PlayerID.ORANGE, new IntelligentDecisionStrategy())};
+
+        //Run the game. The game will end either when a Player reaches 10 victory points or the game reaches the max number of rounds as assigned by maxRounds.
+        //Every round, the game will output to the terminal:
+        //The dice roll
+        //The round and the player's chosen actions
+        //If applicable: an awarding of a player's Longest Road award
+        //The number of victory points for each player
+        
+        //Note that due to this simulation not implementing trading, development cards, etc. there is a possibility that the players may choose to place their starting settlements in areas where
+        //they do not receive the necessary resources to place any pieces down. In this case, rounds may often reach the maxRounds threshold without a winner. This is to be expected.
+        
+        int maxRounds = 8192;
+        Game game = new Game(players, board, validator, dice, stateMachine, maxRounds);
+        game.addVisualizer(new GamePythonVisualizer(board));
+        game.run();
+
+        scanner.close();
     }
 }
